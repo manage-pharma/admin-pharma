4ae5255a86a7fffdb58aaccd6e9ba234408aa68d
nguyenphucminh <59383987+nguyenphucminh@users.noreply.github.com>
2023-03-08 09:32:10 +0700

    fix: success delete

--- src/components/DrugStores/MainDrugStores.js
+++ src/components/DrugStores/MainDrugStores.js
@@ -43,7 +43,7 @@ const MainDrugStore =() => {
       dispatch(listDrugStore(keyword))
     }
     // eslint-disable-next-line
-  },[dispatch, successDelete, keyword])
+  },[dispatch, keyword])
 
   return (
     <>
